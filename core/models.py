69725585d41714811fe6b0d06e4c757587eabfc1
Amirkhon <mrxan9909@gmail.com>
2022-11-01 19:11:32 +0500

    comp

--- core/models.py
+++ core/models.py
@@ -1,6 +1,3 @@
-from distutils.command.upload import upload
-from doctest import BLANKLINE_MARKER
-from email.policy import default
 from django.db import models
 from django.contrib.auth import get_user_model
 import uuid
@@ -10,22 +7,35 @@ User = get_user_model()
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField
-    bio = models.TextField(blank = True)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-    location = models.CharField(max_length= 100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
 
-def __str__(self):
-    return self.user.username
-
+    def __str__(self):
+        return self.user.username
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_image')
+    image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
-    
-    def __str__(self) -> str:
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
         return self.user
